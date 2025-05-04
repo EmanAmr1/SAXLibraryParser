ad20ec99be358a6fa7d80fb5cebe60323c3be4eb
EmanAmr1 <emanamr543@gmail.com>
2025-05-04 15:55:12 +0300

    Book model

---
+++ SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/Models/Book.java
@@ -0,0 +1,13 @@
+package com.Parsing.SAXLibraryParser.Models;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private String title;
+    private String author;
+    private String year;
+    private Double price;
+
+}
