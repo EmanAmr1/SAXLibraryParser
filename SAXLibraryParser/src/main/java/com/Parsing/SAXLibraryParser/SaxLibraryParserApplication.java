71130b90b305dadd953c20c8e96913015de77b45
EmanAmr1 <emanamr543@gmail.com>
2025-05-04 15:46:30 +0300

    init springboot project

---
+++ SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/SaxLibraryParserApplication.java
@@ -0,0 +1,13 @@
+package com.Parsing.SAXLibraryParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaxLibraryParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaxLibraryParserApplication.class, args);
+	}
+
+}
---
+++ SAXLibraryParser/src/test/java/com/Parsing/SAXLibraryParser/SaxLibraryParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Parsing.SAXLibraryParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaxLibraryParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
