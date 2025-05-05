ae32cc83ce8f444738a369200935328fc99901d5
EmanAmr1 <emanamr543@gmail.com>
2025-05-05 10:16:25 +0300

    book xmlFiles parsing

--- SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/Services/Impl/SAXLibraryParserServiceImp.java
+++ SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/Services/Impl/SAXLibraryParserServiceImp.java
@@ -1,13 +1,13 @@
 package com.Parsing.SAXLibraryParser.Services.Impl;
 
+import com.Parsing.SAXLibraryParser.Models.Book;
 import com.Parsing.SAXLibraryParser.Services.SAXLibraryParserService;
 import org.springframework.stereotype.Service;
-import org.xml.sax.helpers.DefaultHandler;
+
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,24 +19,41 @@ public class SAXLibraryParserServiceImp implements SAXLibraryParserService {
 
     public void parse(String path) {
 
+        //Listing files in a directory
         File dir = new File(path);
         File[] files = dir.listFiles();
 
+
+        if(files==null){
+            System.out.println("No XML files found");
+            return;
+        }
+
+        // collect all .xml files in a given directory.
         for (File file : files) {
-            if (file.getAbsolutePath().endsWith(".xml")) {
+            if (file.getAbsolutePath().toLowerCase().endsWith(".xml")) {
                 xmlFiles.add(file);
             }
         }
 
+
         try {
+            //parses xmlFiles using the SAX parser
             for (File xmlfile : xmlFiles) {
                 SAXParserFactory f = SAXParserFactory.newInstance();
                 SAXParser p = f.newSAXParser();
                 SaxParserHandler h = new SaxParserHandler();
                 p.parse(xmlfile, h);
+
+
+                //Prints each Book object
+                List<Book> books = h.getBookList();
+                for (Book book : books) {
+                    System.out.println(book);
+                }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error parsing XML file");
         }
 
     }
--- SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/Services/Impl/SaxParserHandler.java
+++ SAXLibraryParser/src/main/java/com/Parsing/SAXLibraryParser/Services/Impl/SaxParserHandler.java
@@ -1,6 +1,62 @@
 package com.Parsing.SAXLibraryParser.Services.Impl;
 
+import com.Parsing.SAXLibraryParser.Models.Book;
+import lombok.Getter;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class SaxParserHandler extends DefaultHandler {
+
+    Book currentBook = null;
+    List<Book> bookList = new ArrayList<>();
+    StringBuilder currentValue = new StringBuilder();
+
+    @Override
+    public void startElement(String uri, String localName,
+                             String qName, Attributes attributes) throws SAXException {
+
+        if (qName.equalsIgnoreCase("book")) {
+            currentBook = new Book();
+
+        }
+
+        currentValue.setLength(0);
+    }
+
+
+    @Override
+    public void characters(char[] ch, int start, int length) {
+        currentValue.append(ch, start, length);
+    }
+
+
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+        if (currentBook != null) {
+            switch (qName.toLowerCase()) {
+                case "title":
+                    currentBook.setTitle(String.valueOf(currentValue));
+                    break;
+                case "author":
+                    currentBook.setAuthor(String.valueOf(currentValue));
+                    break;
+                case "year":
+                    currentBook.setYear(String.valueOf(currentValue));
+                    break;
+                case "price":
+                    currentBook.setPrice(Double.valueOf(String.valueOf(currentValue)));
+                    break;
+                case "book":
+                    bookList.add(currentBook);
+                    break;
+            }
+
+        }
+    }
+
 }
